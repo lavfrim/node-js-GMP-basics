d76a5f63ba4b3dc15b44289b7ece5b975ec7e55c
Aliaksandr Lazarau <Aliaksandr.Lazarau@coxautoinc.com>
2021-04-05 19:55:48 +0300

    refactor: moved functions to controllers

--- src/tasks/task_2/routers/user.ts
+++ src/tasks/task_2/routers/user.ts
@@ -1,114 +1,25 @@
 import express from 'express';
 import {
-    ReasonPhrases,
-    StatusCodes,
-} from 'http-status-codes';
-import { v4 as uuid } from 'uuid';
-import {
-    ValidatedRequest,
     createValidator,
 } from 'express-joi-validation';
 import { userBodySchema, usersListQuerySchema } from '../validationSchemas';
-import { User, RequestWithInfo, UserRequestSchema, AutoSuggestUsersRequestSchema } from '../types';
-import { mockedUserDataBase } from '../mocks';
-import { getAutoSuggestUsers } from '../utils/getAutoSuggestUsers';
+import { getUser } from '../controllers/getUser';
+import { updateUser } from '../controllers/updateUser';
+import { deleteUser } from '../controllers/deleteUser';
+import { findUser, getAutoSuggestUserList, createUser } from '../controllers/controllersWithMockedDataBase';
 
 export const userRouter = express.Router();
 const validator = createValidator();
-const userDataBase: User[] = [...mockedUserDataBase];
 
-userRouter.param('id', (req: RequestWithInfo, res, next, id) => {
-    const user = userDataBase.find((profile) => profile.id === id);
+userRouter.param('id', findUser);
 
-    if (user) {
-        // mutation as recommend official express docs
-        // http://expressjs.com/en/5x/api.html#app.listen_path_callback
-        // article router.route(path)
-        req.info = { user };
-        next();
-    } else {
-        res.status(StatusCodes.NOT_FOUND);
-        res.json({
-            error: `[${ReasonPhrases.NOT_FOUND}]: There is no such user in the database`,
-        });
-    }
-})
+userRouter.route('/list')
+    .get(validator.query(usersListQuerySchema), getAutoSuggestUserList);
 
-userRouter.get(
-    '/list',
-    validator.query(usersListQuerySchema),
-    (req: ValidatedRequest<AutoSuggestUsersRequestSchema>, res) => {
-    const query = req.query;
-    const autoSuggestUserList = getAutoSuggestUsers({ userDataBase, ...query });
+userRouter.route('/:id')
+    .get(getUser)
+    .patch(validator.body(userBodySchema), updateUser)
+    .delete(deleteUser);
 
-    res.json(autoSuggestUserList);
-});
-
-userRouter.get('/:id', (req: RequestWithInfo, res) => {
-    const { user } = req.info;
-
-    if (user) {
-        res.json(user);
-    }
-});
-
-userRouter.post(
-    '/',
-    validator.body(userBodySchema),
-    (req: ValidatedRequest<UserRequestSchema>, res) => {
-    const isUnique = !!!userDataBase.find((profile) => profile.login === req.body.login);
-
-    if (isUnique) {
-        const newUserProfile: User = {
-            id: uuid(),
-            isDeleted: false,
-            ...req.body,
-        };
-        const isSuccessful = !!userDataBase.push(newUserProfile);
-
-        if (isSuccessful) {
-            res.status(StatusCodes.CREATED);
-            res.json({
-                message: `[${ReasonPhrases.CREATED}]: User was created`,
-                user: newUserProfile,
-            });
-        }
-    } else {
-        res.status(StatusCodes.CONFLICT);
-        res.json({
-            message: `[${ReasonPhrases.CONFLICT}]: Such user already created`,
-        });
-    }
-});
-
-userRouter.patch(
-    '/:id',
-    validator.body(userBodySchema),
-    (req: RequestWithInfo, res) => {
-    const { user } = req.info;
-
-    if (user) {
-        Object.keys(req.body).forEach((prop) => {
-            user[prop] = req.body[prop];
-        })
-        res.status(StatusCodes.CREATED);
-        res.json({
-            message: `[${ReasonPhrases.CREATED}]: User profile was updated`,
-            user: user,
-        });
-    }
-});
-
-userRouter.delete('/:id', (req: RequestWithInfo, res) => {
-    const { user } = req.info;
-
-    if (user) {
-        user.isDeleted = !user.isDeleted;
-
-        res.status(StatusCodes.CREATED);
-        res.json({
-            message: `[${ReasonPhrases.CREATED}]: User was ${user.isDeleted ? 'soft deleted' : 'restored'}`,
-            user: user,
-        });
-    }
-});
+userRouter.route('/')
+    .post(validator.body(userBodySchema), createUser);
