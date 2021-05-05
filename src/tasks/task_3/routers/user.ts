8d4ab42cb1fd946fef995b094606bdbe51eb6811
lavfrim <47713188+lavfrim@users.noreply.github.com>
2021-05-05 13:15:54 +0300

    POSTGRESQL AND LAYERED ARCHITECTURE (#3)
    * feat: add task 3 solution
    
    * feat: add fixes
    
    * fix: gitignore
    
    Co-authored-by: Aliaksandr Lazarau <Aliaksandr.Lazarau@coxautoinc.com>

---
+++ src/tasks/task_3/routers/user.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { getUser, updateUser, deleteUser, createUser, getAutoSuggestUserList } from '../controllers/user';
+import {usersListQuerySchema} from '../validationSchemas';
+import {createValidator} from "express-joi-validation";
+
+export const user = Router();
+const validator = createValidator();
+
+user.route('/list')
+    .get(validator.query(usersListQuerySchema), getAutoSuggestUserList);
+
+user.route('/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser);
+
+user.route('/')
+    .post(createUser);
+
---
+++ src/tasks/task_3/validationSchemas.ts
@@ -0,0 +1,6 @@
+import Joi from 'joi';
+
+export const usersListQuerySchema = Joi.object({
+    loginSubstring: Joi.string().required(),
+    limit: Joi.number().min(1).max(128).required(),
+})
