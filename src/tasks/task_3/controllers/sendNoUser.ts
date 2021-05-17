7484d9abf4b994433b032380a3e52ae4b7ed7445
Aliaksandr Lazarau <Aliaksandr.Lazarau@coxautoinc.com>
2021-05-17 19:31:31 +0300

    feat: init common approach for tasks manage

--- src/tasks/task_3/controllers/sendNoUser.ts
+++
@@ -1,6 +0,0 @@
-import {ReasonPhrases, StatusCodes} from "http-status-codes";
-import { Response } from 'express';
-
-export const sendNoUser = (res: Response) => res.status(StatusCodes.NOT_FOUND).json({
-    error: `[${ReasonPhrases.NOT_FOUND}]: There is no such user in the database`,
-});
